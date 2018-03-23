bbfe77cebf1064c1cf3d983ec90371bb0d883b42
WatchingBoss <opil94@yandex.ru>
2018-03-23 14:17:12 +0300

    Changes

--- Windows/file_manager/misc/shell.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-set path=F:\Documents\Projects_C\LearningC_Win\Windows\file_manager\misc;%path%
