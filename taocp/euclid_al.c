ea7e7b3a040362aefcfdb73f9271d5cf60e86be6
WatchingBoss <opil94@yandex.ru>
2018-04-16 00:01:25 +0300

    euclid_al

--- taocp/euclid_al.c
+++ taocp/euclid_al.c
@@ -1,23 +1,6 @@
 #include <stdio.h>
 
-void algo(int *m, int *n)
-{
-	int r, m1 = *m, n1 = *n;
-	while (r >= 0)
-    {
-		r = m1 % n1;
-		if (r == 0)
-		{
-			printf("%d is the greatest common divison of %d and %d\n", n1, *m, *n);
-			break;
-		}
-		else
-		{
-			m1 = n1;
-			n1 = r;
-		}
-    }
-}
+void algo(int, int);
 
 int main()
 {
@@ -26,7 +9,26 @@ int main()
 	printf("Enter m and n integers: ");
 	scanf("%d %d", &m, &n);
 
-	algo(&m, &n);
+	algo(m, n);
 
 	return 0;
+}
+
+void algo(int m, int n)
+{
+	int r, m1 = m, n1 = n;
+	while (r >= 0)
+    {
+		r = m1 % n1;
+		if (r == 0)
+		{
+			printf("%d is the greatest common divison of %d and %d\n", n1, m, n);
+			break;
+		}
+		else
+		{
+			m1 = n1;
+			n1 = r;
+		}
+    }
 }
