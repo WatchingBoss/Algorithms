5ca4771aac6d1c38e92354cde2096cf30a3a347a
Watchingboss <opil94@yandex.ru>
2018-05-13 14:51:59 +0300

    It's funny but I cannot figure out how works this algorithm. Will try

---
+++ Some/factorial/f_array.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int multi(int, int *, int);
+void fact(const int *);
+
+int main()
+{
+	int number;
+
+	printf("Maximum count of digits in number is %d\n\n", MAX);
+
+	printf("Enter your number and this pretty cool program will compute factorial\n"
+		   "LMAO :))  : ");
+	scanf("%d", &number);
+
+	fact(&number);
+
+	return(0);
+}
+
+void fact(const int *n)
+{
+	int res[MAX];
+
+	res[0] = 1;
+	int resSize = 1;
+
+	for(int i = 2; i <= *n; ++i)
+		resSize = multi(i, res, resSize);
+
+	printf("Factorial of %d is ", *n);
+	for(int i = resSize - 1; i >= 0; --i)
+		printf("%d", res[i]);
+	printf("\n");
+}
+
+int multi(int x, int *res, int resSize)
+{
+	int carry = 0;
+
+	for(int i = 0; i < resSize; ++i)
+	{
+		int prod = res[i] * x + carry;
+
+		res[i] = prod % 10;
+
+		carry = prod / 10;
+	}
+
+	while(carry)
+	{
+		res[resSize] = carry % 10;
+		carry = carry / 10;
+		++resSize;
+	}
+
+	return(resSize);
+}
