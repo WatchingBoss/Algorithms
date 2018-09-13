6180f6a456aaad733b096458f446b971ee25514f
WatchingBoss <opil94@yandex.ru>
2018-09-13 19:38:57 +0300

    Although define second variant for insertion sort, but it works slower

--- sort/insertion_sort/is_1.c
+++ sort/insertion_sort/is_1.c
@@ -23,6 +23,22 @@ void print_arr(size_t arr[], size_t size)
 	printf("\n");
 }
 
+/* Slower */
+void i_sort_2(size_t arr[], size_t size)
+{
+	for(size_t i = 2; i < size; ++i)
+	{
+		size_t j = 1;
+		while(arr[i] > arr[j])
+			++j;
+		size_t m = arr[i];
+		for(size_t k = 0; (int)k < (int)i - (int)j - 1; ++k)
+			arr[i - k] = arr[i - k - 1];
+		arr[j] = m;
+	}
+}
+
+/* Faster */
 void i_sort(size_t arr[], size_t size)
 {
 	int key = 0, j = 0;
@@ -54,12 +70,13 @@ int main()
 	for(int i = 0; i < NUM; ++i)
 		arr[i] = rand() % (MAX + 1 - MIN) + MIN;
 
-	time_t begin = time(NULL);
+	clock_t begin = clock();
 	i_sort(arr, NUM);
-	time_t end = time(NULL);
+	clock_t end = clock();
 
-	print_arr(arr, NUM);
-	printf("%ld second to sort %d elements\n", end - begin, NUM);
+//	print_arr(arr, NUM);
+	printf("%.5f second to sort %d elements\n",
+		   ((float)end - (float)begin) / 1000000.f, NUM);
 
 	return 0;
 }
